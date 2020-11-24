68b11d5beddeb2d7ff3e85009dc91ba91cdc436a
Jose Torres <torresjose6541@gmail.com>
2020-11-24 14:27:39 -0800

    Initial commit, may edit this to go by desired weeks of inventory

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# scuffed_GoogleAppsScript
+### This is a repo for my scuffed google apps script.
+---
+Essentially all it does is get data from a Google spreadsheet
+it formats it and sends a daily reminder email.
---
+++ index.js
@@ -0,0 +1,66 @@
+const sendEmailNotification = (email = 'myemailhere@gmail.com', subject = 'Whatever you want', skuArray) => {
+    
+    const template = HtmlService.createTemplateFromFile('InventoryDigest')
+    template.data = skuArray;
+    const message = template.evaluate().getContent();
+    
+    MailApp.sendEmail({
+    to: email,
+    subject: subject,
+    htmlBody: message
+    });
+  
+}
+
+let master = [];
+  
+const populateSKUArray = () => {
+    const skus = SpreadsheetApp.getActiveSpreadsheet().getSheetByName('MasterSKUsCalendar').getRange('C2:C30').getValues();
+    const inventories = SpreadsheetApp.getActiveSpreadsheet().getSheetByName('MasterSKUsCalendar').getRange('I2:I30').getValues();
+    const suggested = SpreadsheetApp.getActiveSpreadsheet().getSheetByName('MasterSKUsCalendar').getRange('O2:O30').getValues();
+    const reOrderDates = SpreadsheetApp.getActiveSpreadsheet().getSheetByName('MasterSKUsCalendar').getRange('P2:P30').getValues();
+    let allData = [...skus, ...inventories, ...suggested, ...reOrderDates]; //Not needed
+    skus.map(sku => {
+      let [title] = sku;
+      master.push({name: title})
+    })
+  
+    inventories.map((inven, index) => {
+      let [current] = inven;
+      for (let i = 0; i < master.length; i++) {
+          if (i === index) {
+            master[i] = Object.assign(master[i], {currentInventory: current})
+          }
+      }
+    })
+  
+    suggested.map((sugg, index) => {
+      let [suggested] = sugg;
+      for (let i = 0; i < master.length; i++) {
+          if (i === index) {
+            master[i] = Object.assign(master[i], {reOrderPoint: suggested})
+          }
+      }
+     })
+  
+     reOrderDates.map((date, index) => {
+      let [datetime] = date;
+      for (let i = 0; i < master.length; i++) {
+          if (i === index) {
+            master[i] = Object.assign(master[i], {reOrderDate: String(datetime).split('').slice(0,10).join('') + ' 2020'})
+          }
+      }
+     })
+
+     let emailList = master.filter(sku => typeof sku.reOrderPoint === 'number' 
+     && sku.reOrderPoint !== 0 && sku.reOrderPoint !== undefined)
+     let now = new Date();
+     let secondList = master.filter(sku => sku.reOrderDate < now)
+     Logger.log(emailList, secondList) //Logger.log is scuffed console.log
+     return emailList;
+}
+  
+const main = () => { //This is run daily by a trigger handled by GAS api
+    const data = populateSKUArray();
+    sendEmailNotification('myemail@gmail.com', 'Whatever you want', data);
+}
