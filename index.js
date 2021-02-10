a0a8d1f86366101123114f548fc25cdfaf83777f
Jose Torres <49792162+JTorresTMP@users.noreply.github.com>
2021-02-10 08:55:06 -0800

    Update index.js

--- index.js
+++ index.js
@@ -15,50 +15,55 @@ const sendEmailNotification = (email = 'myemailhere@gmail.com', subject = 'Whate
 let master = [];
   
 const populateSKUArray = () => {
-    const skus = SpreadsheetApp.getActiveSpreadsheet().getSheetByName('MasterSKUsCalendar').getRange('C2:C30').getValues();
-    const inventories = SpreadsheetApp.getActiveSpreadsheet().getSheetByName('MasterSKUsCalendar').getRange('I2:I30').getValues();
-    const suggested = SpreadsheetApp.getActiveSpreadsheet().getSheetByName('MasterSKUsCalendar').getRange('O2:O30').getValues();
-    const reOrderDates = SpreadsheetApp.getActiveSpreadsheet().getSheetByName('MasterSKUsCalendar').getRange('P2:P30').getValues();
-    let allData = [...skus, ...inventories, ...suggested, ...reOrderDates]; //Not needed
-    skus.map(sku => {
-      let [title] = sku; //need to destructure due to the way Google Sheets handles data
-      master.push({name: title})
-    })
-  
-    inventories.map((inven, index) => {
-      let [current] = inven;
-      for (let i = 0; i < master.length; i++) {
-          if (i === index) {
-            //Object spread operator does not work on Google Apps Scripts
-            master[i] = Object.assign(master[i], {currentInventory: current})
-          }
-      }
-    })
-  
-    suggested.map((sugg, index) => {
-      let [suggested] = sugg;
-      for (let i = 0; i < master.length; i++) {
-          if (i === index) {
-            master[i] = Object.assign(master[i], {reOrderPoint: suggested})
-          }
-      }
-     })
-  
-     reOrderDates.map((date, index) => {
-      let [datetime] = date;
-      for (let i = 0; i < master.length; i++) {
-          if (i === index) {
-            master[i] = Object.assign(master[i], {reOrderDate: String(datetime).split('').slice(0,10).join('') + ' 2020'})
-          }
-      }
-     })
+  const skus = SpreadsheetApp.getActiveSpreadsheet().getSheetByName('MasterSKUsCalendar').getRange('C2:C30').getValues();
+  const inventories = SpreadsheetApp.getActiveSpreadsheet().getSheetByName('MasterSKUsCalendar').getRange('I2:I30').getValues();
+  const suggested = SpreadsheetApp.getActiveSpreadsheet().getSheetByName('MasterSKUsCalendar').getRange('O2:O30').getValues();
+  const reOrderDates = SpreadsheetApp.getActiveSpreadsheet().getSheetByName('MasterSKUsCalendar').getRange('P2:P30').getValues();
+  let allData = [...skus, ...inventories, ...suggested, ...reOrderDates]; //Not needed
+  skus.map(sku => {
+    let [title] = sku;
+    master.push({name: title})
+  })
 
-     let emailList = master.filter(sku => typeof sku.reOrderPoint === 'number' 
-     && sku.reOrderPoint !== 0 && sku.reOrderPoint !== undefined)
-     let now = new Date();
-     let secondList = master.filter(sku => sku.reOrderDate < now)
-     Logger.log(emailList, secondList) //Logger.log is scuffed console.log
-     return emailList;
+  Logger.log(inventories)
+  inventories.map((inven, index) => {
+    let [current] = inven;
+    for (let i = 0; i < master.length; i++) {
+        if (i === index) {
+          master[i] = Object.assign(master[i], {currentInventory: numFormatter(Number(current))})
+        }
+    }
+  })
+
+  Logger.log('Pre')
+  Logger.log(suggested)
+  suggested.map((sugg, index) => {
+    let [suggested] = sugg;
+    for (let i = 0; i < master.length; i++) {
+        if (i === index) {
+          master[i] = Object.assign(master[i], {reOrderPoint: suggested})
+        }
+    }
+   })
+  Logger.log('Post')
+  Logger.log(suggested)
+
+   reOrderDates.map((date, index) => {
+    let [datetime] = date;
+    for (let i = 0; i < master.length; i++) {
+        if (i === index) {
+          master[i] = Object.assign(master[i], {reOrderDate: String(datetime).split('').slice(0,10).join('') + ' 2020'})
+        }
+    }
+   })
+   let emailList = master.filter(sku => typeof sku.reOrderPoint === 'number' && sku.reOrderPoint !== 0 && sku.reOrderPoint !== undefined)
+   let now = new Date();
+   let secondList = master.filter(sku => sku.reOrderDate < now)
+   emailList.forEach(obj => {
+     obj.reOrderPoint = numFormatter(obj.reOrderPoint)
+   })
+   Logger.log(emailList)
+   return emailList;
 }
   
 const main = () => { //This is run daily by a trigger handled by GAS api
